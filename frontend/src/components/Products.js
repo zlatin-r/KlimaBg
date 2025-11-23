5d89e305090dd6459ea767af052426192f7ff3b3
zlatin-r <zlatinrusev@gmail.com>
2025-11-23 17:09:04 +0200

    solution added

--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -14,7 +14,6 @@ const Products = () => {
         setLoading(false);
       })
       .catch((err) => {
-      }
         console.error("Error fetching products:", err);
         setLoading(false);
       });
