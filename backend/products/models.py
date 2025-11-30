b092fc5458d2297f9474fff93e9f72caaf16fb6d
zlatin-r <zlatinrusev@gmail.com>
2025-11-30 10:13:13 +0200

    feat: Add product serializers, update backend configurations, and integrate frontend components and pages

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    in_stock = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='products/')
+    category = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def product_list(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True, context={'request': request})
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def product_detail(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+    except Product.DoesNotExist:
+        return Response({'error': 'Product not found'}, status=404)
+
+    serializer = ProductSerializer(product, context={'request': request})
+    return Response(serializer.data)
+
