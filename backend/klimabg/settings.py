bea17bbf54f71c62a3b32520c92929d1f1358ad5
zlatin-r <zlatinrusev@gmail.com>
2025-11-30 09:59:24 +0200

    feat: Initialize frontend and integrate product URLs

--- backend/klimabg/settings.py
+++ backend/klimabg/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest-framework',
+    'rest_framework',
     'corsheaders',
 ] + USER_APPS
 
@@ -93,9 +93,13 @@ WSGI_APPLICATION = 'klimabg.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "klimabg",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
