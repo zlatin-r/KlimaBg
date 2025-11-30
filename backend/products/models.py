d0b34cf6c401e0f4c8efed07337c8047b320ca9c
zlatin-r <zlatinrusev@gmail.com>
2025-11-30 12:41:26 +0200

    feat: Add product serializers, URLs, and initial migration

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    model = models.CharField(max_length=200)
+    manufacturer = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    in_stock = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.manufacturer} - {self.model}"
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+class ProductList(APIView):
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
