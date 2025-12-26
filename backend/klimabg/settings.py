f6c22272233cd444d9b7ad03770446f2c1e48eec
zlatin-r <zlatinrusev@gmail.com>
2025-12-26 14:28:20 +0200

    commit

--- backend/klimabg/settings.py
+++ backend/klimabg/settings.py
@@ -1,8 +1,5 @@
-import environ
+from decouple import config
 from pathlib import Path
-
-env = environ.Env()
-environ.Env.read_env()  # reads .env file
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -13,7 +10,7 @@ PROJECT_ROOT = BASE_DIR.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8@1ii7fs(vbzuv!pb0ag!g1-4m00p5ju+w3k5i8^5rk#5)xlca'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -148,6 +145,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
